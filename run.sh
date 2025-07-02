7bf72cd140e75d0f3d378b430ec004f02a2bbbf4
kailash Karki <kailash.karki@presight.ai>
2025-07-02 20:07:52 +0400

    test

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ curl -sfL https://get.k3s.io | sh -
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' | sudo tee /etc/profile.d/k3s.sh
 chmod +x /etc/profile.d/k3s.sh
-source /etc/profile
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 sleep 5
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> ~/.bashrc
@@ -13,3 +12,4 @@ helm install supereye-core supereye-core
 helm install emqx emqx
 helm install alert-dashboard alert-dashboard
 echo 'helm installed successfully'
+source /etc/profile
