fc56bb80b1d09b4062c8fc0826f02dc79d88299c
kailash Karki <kailash.karki@presight.ai>
2025-07-23 16:35:12 +0400

    path fix

--- instance-defualt-script.sh
+++ instance-defualt-script.sh
@@ -1,7 +1,6 @@
 sudo su
 sudo apt update
 sudo apt install -y git
-sh disk_attachment.sh
 cd /opt/
 git clone https://github.com/karkig/supereye-helm.git
 cd supereye-helm
--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 sudo su
+sh disk_attachment.sh
 curl -sfL https://get.k3s.io | sh -
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' | sudo tee /etc/profile.d/k3s.sh
