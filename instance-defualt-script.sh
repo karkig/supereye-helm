5da2506f5dd28bc32edfaf0e28e422b68db3c23b
kailash Karki <kailash.karki@presight.ai>
2025-07-23 15:36:23 +0400

    fix code

--- instance-defualt-script.sh
+++ instance-defualt-script.sh
@@ -1,8 +1,8 @@
 sudo su
 sudo apt update
 sudo apt install -y git
+sh disk_attachment.sh
 cd /opt/
 git clone https://github.com/karkig/supereye-helm.git
 cd supereye-helm
-sh disk_attachment.sh
 sh run.sh
